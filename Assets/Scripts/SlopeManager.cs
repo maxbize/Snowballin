00ee47fee380a13386cfb86d669bbca6999dae6d
Frenchie14 <mbize2005@gmail.com>
2015-12-12 02:26:23 -0800

    Basic Slope Streaming

--- Assets/Scripts/SlopeManager.cs
+++ Assets/Scripts/SlopeManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SlopeManager : MonoBehaviour {
 
@@ -9,29 +10,43 @@ public class SlopeManager : MonoBehaviour {
 
     private SlopeSlice playerSlice;
     public Vector3 toGround { get; private set; } // This doesn't really belong here...
+    public Vector3 alongGround { get; private set; } // This doesn't really belong here...
+    private Dictionary<Vector3, SlopeSlice> sliceMap = new Dictionary<Vector3, SlopeSlice>();
 
 	// Use this for initialization
 	void Start () {
         toGround = Quaternion.Euler(FindObjectOfType<SlopeSliceGenerator>().slopeAngle + 90, 0, 0) * Vector3.forward;
+        alongGround = Quaternion.Euler(FindObjectOfType<SlopeSliceGenerator>().slopeAngle, 0, 0) * Vector3.forward;
         sliceGen.MakeMeshSlice(null, null, null, null);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    // There should always be slope in a cone in front of the player
-        GenSlice(player.transform.position + Vector3.forward * 10);
+        GenSlice(player.transform.position + alongGround * sliceGen.sliceLength);
+        GenSlice(player.transform.position + Vector3.right * sliceGen.sliceWidth);
+        GenSlice(player.transform.position - Vector3.right * sliceGen.sliceWidth);
     }
 
     // pos just needs to be somewhere on the slice. Generates a slice if none found
     private void GenSlice(Vector3 pos) {
         if (FindSlice(pos, 100) == null) {
-            sliceGen.MakeMeshSlice(null, null, player.currentSlice, null);
+            SlopeSlice left = FindSlice(pos - Vector3.right * sliceGen.sliceWidth, 100);
+            SlopeSlice back = FindSlice(pos - alongGround * sliceGen.sliceLength, 100);
+            if (back == null) {
+                back = FindSlice(pos - alongGround * sliceGen.sliceLength * 0.5f, 100);
+            }
+            SlopeSlice right = FindSlice(pos + Vector3.right * sliceGen.sliceWidth, 100);
+
+            sliceGen.MakeMeshSlice(null, left, back, right);
         }
     }
 
     public SlopeSlice FindSlice(Vector3 origin, float distance) {
         RaycastHit hit;
         if (Physics.Raycast(origin, toGround * distance, out hit)) {
+            return hit.transform.GetComponent<SlopeSlice>();
+        } else if (Physics.Raycast(origin, -1 * (toGround * distance), out hit)) {
             return hit.transform.GetComponent<SlopeSlice>();
         }
         return null;
