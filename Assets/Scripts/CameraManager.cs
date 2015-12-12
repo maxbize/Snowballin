47012bd742286b4ec4fd46fb8991284522d1f61b
Frenchie14 <mbize2005@gmail.com>
2015-12-11 19:36:30 -0800

    Basic snowball movement + Camera

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+    // Set in editor
+    public GameObject focus;
+    public float distance;
+    public float angle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = Quaternion.Euler(angle, 0, 0) * Vector3.forward;
+        offset *= distance;
+        transform.position = focus.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    // Set in editor
+    public float moveSpeed;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 input = Vector3.zero;
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            input += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            input -= Vector3.right;
+        }
+        Move(input);
+	}
+
+    private void Move(Vector3 dir) {
+        rb.AddForce(dir * moveSpeed * rb.mass);
+    }
+
+}
