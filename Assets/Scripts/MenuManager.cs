00929697a0df933c72866d7b4efed5ea35f6b1d0
Frenchie14 <mbize2005@gmail.com>
2015-12-13 14:33:05 -0800

    Menu embedded in GameScene

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class MenuManager : MonoBehaviour {
 
+    // Set in editor
+    public GameObject player;
+    public GameObject gui;
+    public GameObject controlsUi;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,8 +15,14 @@ public class MenuManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
-            Application.LoadLevel("GameScene");
+        if (player.activeSelf) {
+            gameObject.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            player.SetActive(true);
+            gui.SetActive(false);
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            controlsUi.SetActive(!controlsUi.activeSelf);
         }
 	}
 }
